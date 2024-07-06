da51028835607a16d662b4b969a002cc4e0ff121
Nkot117 <kumakura117@gmail.com>
2024-07-06 22:00:04 +0900

    スクリーンショット制限、スクリーンショット検出を実装

--- app/src/main/java/com/example/capturerestrictionsample/MainActivity.kt
+++ app/src/main/java/com/example/capturerestrictionsample/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.example.capturerestrictionsample
 
+import android.app.Activity.ScreenCaptureCallback
+import android.os.Build
 import android.os.Bundle
+import android.util.Log
+import android.view.WindowManager
+import android.view.WindowManager.SCREEN_RECORDING_STATE_VISIBLE
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
+
 class MainActivity : AppCompatActivity() {
+    val screenCaptureCallback = ScreenCaptureCallback {
+        Log.d("CaptureRestrictionSample", "スクリーンショットを検出")
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +26,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE)
     }
+
+    override fun onStart() {
+        super.onStart()
+
+        // OS 14以上の場合に使用できる機能
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            registerScreenCaptureCallback(mainExecutor, screenCaptureCallback)
+        }
+    }
+    override fun onStop() {
+        super.onStop()
+        // OS 14以上の場合に使用できる機能
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            unregisterScreenCaptureCallback(screenCaptureCallback)
+        }
+    }
+
 }
